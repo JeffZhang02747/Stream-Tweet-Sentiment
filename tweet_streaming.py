5bf33b1423473977c13ac4d4063290ad9265149b
Jeff Zhang <a920249925@gmail.com>
2016-01-04 20:49:42 -0500

    set config file

--- tweet_streaming.py
+++ tweet_streaming.py
@@ -6,29 +6,19 @@ from learner import sentimentLearner
 from jubatus.classifier.client import Classifier
 
 from requests.packages.urllib3.exceptions import ProtocolError
-
-
-#Variables that contains the user credentials to access Twitter API 
-access_token = "2209821403-BWDoRCLfPajwINfEN0IwWkWLjGGaLvCcsutiM0U"
-access_token_secret = "bsal054FCg54qZuPkmMpuFgLGK7cmKYEGwRpJFn1684DY"
-consumer_key = "NWYG4K5rjtHSZL7qHLlPDxG9m"
-consumer_secret = "DdMR9irroK8h3bMsekswA0JjGYV94EVp3CDsuUsLTRW3Sm6CTY"
-
+from config import *
 
 #Jubatus
 host = '127.0.0.1'
 port = 9199
 name = 'sentimentModel'
 
-
 happy_emoticons = emoji_filter(100, True, 0.49)
 sad_emoticons = emoji_filter(100, False, -0.1)
-
 emoticon_list = happy_emoticons + sad_emoticons
 
 #override tweepy.StreamListener to add logic to on_status
 class MyStreamListener(tweepy.StreamListener):
-
     def __init__(self,learner):
         super(MyStreamListener,self).__init__()
         self.learner = learner
@@ -37,7 +27,6 @@ class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
         tweet = status.text
         self.learner.trainTweet(tweet)
-
 
 if __name__ == '__main__':
 
